04a069003e31b0280f19c5b7bad7e5a211fd1c84
HananSalemAlqarni <hsh7al@gmail.com>
2022-09-05 17:28:19 +0300

    new

---
+++ src/main/java/com/codingdojo/Hellohanan/HelloHananApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Hellohanan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHananApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHananApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/Hellohanan/HelloHananApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Hellohanan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHananApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
